b20fe1ea0b9fb2b45abbcfb628c3f585690e34b3
lefeb <lefebvr.bat@gmail.com>
2024-11-03 15:15:55 +0100

    add messages

--- src/main/kotlin/com/example/cleancodepluginandjava/actions/ImportViewCleanAction.kt
+++ src/main/kotlin/com/example/cleancodepluginandjava/actions/ImportViewCleanAction.kt
@@ -8,13 +8,17 @@ import com.example.cleancodepluginandjava.core.tasks.ImportViewCleanTask
 import com.example.cleancodepluginandjava.core.utils.EditFileUtils
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.ui.Messages
 
 class ImportViewCleanAction : AnAction("Import View Clean"){
     override fun actionPerformed(e: AnActionEvent) {
         var contents = getContents(e)
+        Messages.showMessageDialog(getPath(e), "path", Messages.getInformationIcon())
+        Messages.showMessageDialog(contents.toString(), "Avant Modif", Messages.getInformationIcon())
         contents = ImportViewCleanTask.importViewCleanTask(contents)
+        Messages.showMessageDialog(contents.toString(), "Apres Modif", Messages.getInformationIcon())
         EditFileUtils.setContents(getPath(e), contents)
-        
+
     }
 
 }
